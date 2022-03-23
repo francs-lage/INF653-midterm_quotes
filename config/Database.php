5a5cca1fed2519fa98f76eaf2b908eec766a8621
francs-lage <fnlage@mail.fhsu.edu>
2022-03-23 16:58:41 -0700

    Revise Database HerokuV3: variable name was mismatching

--- config/Database.php
+++ config/Database.php
@@ -1,31 +1,29 @@
-<?php
-    class Database {
-        private $url;
-        private $conn;
-
-        function __construct(){
-            $this->conn = null;
-            $this->url = getenv('JAWSDB_URL');
-        }
-
-        //Database Connect
-        public function connect(){
-
-            $dbparts = parse_url($this->url);
-            
-            $host = $dbparts['host'];
-            $dbname = ltrim($dbparts['path'], '/');
-            $username = $dbparts['user'];
-            $password = $dbparts['pass'];
-
-            try {
-                $this->conn = new PDO('mysql:host=' . $host . ';dbname=' . $dbname, $username, $password);
-                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-            }catch(PDOException $e){
-                echo 'Connection Error: ' . $e->getMessage();
-            }
-
-            return $this->conn;
+<?php 
+// The main part of this code comes from the PDO-intro Week 6 assignment
+class Database{
+    private $connection;
+    private $host;
+    private $db_name;
+    private $username;
+    private $password;
+    
+    function __construct(){
+        $url     = getenv('JAWSDB_URL');
+        $dbparts = parse_url($url);
+        $this->$host     = $dbparts['host'];
+        $this->$db_name  = ltrim($dbparts['path'], '/');
+        $this->$username = $dbparts['user'];
+        $this->$password = $dbparts['pass'];
+    }
+    public function connect(){
+        try {
+            $this->connection = new PDO("mysql:host={$this->$host};dbname={$this->$db_name}", $this->$username, $this->$password);
+            $this->connection = setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            $error_message = 'Database Error: ';
+            $error_message .= $e->getMessage();
+            echo $error_message;
+            exit('Unable to connect to the database');
         }
     }
+}
