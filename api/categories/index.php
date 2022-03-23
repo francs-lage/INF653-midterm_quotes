a73871aab1ebd69d33f1caa30778718a5c0dca5d
francs-lage <fnlage@mail.fhsu.edu>
2022-03-23 12:12:17 -0700

    index fixed to test methods

--- api/categories/index.php
+++ api/categories/index.php
@@ -7,6 +7,15 @@ if ($method === 'OPTIONS') {
     header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
 }
 
-// Verify for arguments in the link
-if (isset($_GET['id'])) include 'read_single.php';
-else include 'read.php';
+// Verify the method called
+$method = $_SERVER['REQUEST_METHOD'];
+if     ($method == 'POST'  ) include 'create.php';
+elseif ($method == 'PUT'   ) include 'update.php';
+elseif ($method == 'DELETE') include 'delete.php';
+elseif ($method == 'GET'   ){
+
+    // Verify for arguments in the link case method is GET
+    if (isset($_GET['id'])) include 'read_single.php';
+    else include 'read.php';
+}
+else echo json_encode(array('message' => 'Fail to Identify Method'));
--- api/quotes/index.php
+++ api/quotes/index.php
@@ -7,17 +7,25 @@ if ($method === 'OPTIONS') {
     header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
 }
 
-// Verify which arguments are in the link
-if (isset($_GET['id'])) $varId = true;
-else $varId = false;
-if (isset($_GET['authorId'])) $varAuthorId = true;
-else $varAuthorId = false;
-if (isset($_GET['categoryId'])) $varCategoryId = true;
-else $varCategoryId = false;
+$method = $_SERVER['REQUEST_METHOD'];
+if     ($method == 'POST'  ) include 'create.php';
+elseif ($method == 'PUT'   ) include 'update.php';
+elseif ($method == 'DELETE') include 'delete.php';
+elseif ($method == 'GET'   ){
 
-// route to a specific file according to the combination of arguments
-if ($varId) include 'read_single.php';
-elseif ( $varAuthorId && !$varCategoryId) include 'read_author.php';
-elseif (!$varAuthorId &&  $varCategoryId) include 'read_category.php';
-elseif ( $varAuthorId &&  $varCategoryId) include 'read_comb.php';
-else include 'read.php';
+    // Verify which arguments are in the link
+    if (isset($_GET['id'])) $varId = true;
+    else $varId = false;
+    if (isset($_GET['authorId'])) $varAuthorId = true;
+    else $varAuthorId = false;
+    if (isset($_GET['categoryId'])) $varCategoryId = true;
+    else $varCategoryId = false;
+
+    // route to a specific file according to the combination of arguments
+    if ($varId) include 'read_single.php';
+    elseif ( $varAuthorId && !$varCategoryId) include 'read_author.php';
+    elseif (!$varAuthorId &&  $varCategoryId) include 'read_category.php';
+    elseif ( $varAuthorId &&  $varCategoryId) include 'read_comb.php';
+    else include 'read.php';
+}
+else echo json_encode(array('message' => 'Fail to Identify Method'));
