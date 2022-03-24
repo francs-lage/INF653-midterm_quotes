18393a7ef769c620b8fadc0810340968bd92de63
francs-lage <fnlage@mail.fhsu.edu>
2022-03-23 18:34:27 -0700

    Back to Database HEROKU.v3 change  to

--- config/Database.php
+++ config/Database.php
@@ -1,31 +1,39 @@
-<?php
-    class Database {
-        private $url;
-        private $conn;
-
-        function __construct(){
-            $this->conn = null;
-            $this->url = getenv('JAWSDB_URL');
-        }
-
-        //Database Connect
-        public function connect(){
-
-            $dbparts = parse_url($this->url);
-            
-            $host = $dbparts['host'];
-            $dbname = ltrim($dbparts['path'], '/');
-            $username = $dbparts['user'];
-            $password = $dbparts['pass'];
-
-            try {
-                $this->conn = new PDO('mysql:host=' . $host . ';dbname=' . $dbname, $username, $password);
-                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-            }catch(PDOException $e){
-                echo 'Connection Error: ' . $e->getMessage();
-            }
-
-            return $this->conn;
-        }
+<?php 
+// The main part of this code comes from the PDO-intro Week 6 assignment
+class Database{
+    private $conn;
+    private $url;
+    //private $host;
+    //private $database;
+    //private $username;
+    //private $password;
+    
+    function __construct(){
+        $this->conn = null;
+        $this->$url       = getenv('JAWSDB_URL');
+        //$dbparts = parse_url($url);
+        //$this->$host     = $dbparts['host'];
+        //$this->$database  = ltrim($dbparts['path'], '/');
+        //$this->$username = $dbparts['user'];
+        //$this->$password = $dbparts['pass'];
     }
+    public function connect(){
+
+        $dbparts = parse_url($this->url);
+
+        $host     = $dbparts['host'];
+        $dbname   = ltrim($dbparts['path'], '/');
+        $username = $dbparts['user'];
+        $password = $dbparts['pass'];
+        try {
+            $this->conn = new PDO('mysql:host=' . $host . ';dbname=' . $dbname, $username, $password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            $error_message = 'Database Error: ';
+            $error_message .= $e->getMessage();
+            echo $error_message;
+            exit(' -- Unable to connect to the database.');
+        }
+        return $this->conn;
+    }
+}
