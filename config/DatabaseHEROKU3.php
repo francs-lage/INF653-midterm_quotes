6b7b8b4c073758e88873e3eb3f0f47a7648f6134
francs-lage <fnlage@mail.fhsu.edu>
2022-03-23 18:18:29 -0700

    Trying different Database file

---
+++ config/DatabaseHEROKU3.php
@@ -0,0 +1,39 @@
+<?php 
+// The main part of this code comes from the PDO-intro Week 6 assignment
+class Database{
+    private $connection;
+    private $url;
+    //private $host;
+    //private $database;
+    //private $username;
+    //private $password;
+    
+    function __construct(){
+        $this->connection = null;
+        $this->$url       = getenv('JAWSDB_URL');
+        //$dbparts = parse_url($url);
+        //$this->$host     = $dbparts['host'];
+        //$this->$database  = ltrim($dbparts['path'], '/');
+        //$this->$username = $dbparts['user'];
+        //$this->$password = $dbparts['pass'];
+    }
+    public function connect(){
+
+        $dbparts = parse_url($this->url);
+
+        $host     = $dbparts['host'];
+        $dbname   = ltrim($dbparts['path'], '/');
+        $username = $dbparts['user'];
+        $password = $dbparts['pass'];
+        try {
+            $this->connection = new PDO('mysql:host=' . $host . ';dbname=' . $dbname, $username, $password);
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            $error_message = 'Database Error: ';
+            $error_message .= $e->getMessage();
+            echo $error_message;
+            exit(' -- Unable to connect to the database.');
+        }
+        return $this->connection;
+    }
+}
