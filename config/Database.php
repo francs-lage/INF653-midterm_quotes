4b41e7b11a0ac2c89a6711c76ee9e23263ba4460
francs-lage <fnlage@mail.fhsu.edu>
2022-03-23 19:21:23 -0700

    Modified Database HEROKU v3

--- config/Database.php
+++ config/Database.php
@@ -3,30 +3,30 @@
 class Database{
     private $connection;
     private $url;
-    // private $host;
-    // private $database;
-    // private $username;
-    // private $password;
+    private $host;
+    private $database;
+    private $username;
+    private $password;
     
     function __construct(){
         $this->connection = null;
         $this->url = getenv('JAWSDB_URL');
-        // $dbparts = parse_url($this->url);
-        // $this->$host     = $dbparts['host'];
-        // $this->$database  = ltrim($dbparts['path'], '/');
-        // $this->$username = $dbparts['user'];
-        // $this->$password = $dbparts['pass'];
+        $dbparts = parse_url($this->url);
+        $this->$host     = $dbparts['host'];
+        $this->$database  = ltrim($dbparts['path'], '/');
+        $this->$username = $dbparts['user'];
+        $this->$password = $dbparts['pass'];
     }
     public function connect(){
         // $this->conn = null;
         // $this->url       = getenv('JAWSDB_URL');
-        $dbparts  = parse_url($this->url);
-        $host     = $dbparts['host'];
-        $dbname   = ltrim($dbparts['path'], '/');
-        $username = $dbparts['user'];
-        $password = $dbparts['pass'];
+        // $dbparts  = parse_url($this->url);
+        // $host     = $dbparts['host'];
+        // $dbname   = ltrim($dbparts['path'], '/');
+        // $username = $dbparts['user'];
+        // $password = $dbparts['pass'];
         try {
-            $this->connection = new PDO('mysql:host=' . $host . ';dbname=' . $dbname, $username, $password);
+            $this->connection = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->dbname, $this->username, $this->password);
             $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
             $error_message .= $e->getMessage();
